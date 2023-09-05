06ad9d6790b3ae8440217ee12981f31cc9683034
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-09-05 19:40:55 +0300

    Setup Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/{page}', [AdminController::class,'index']);
+Auth::routes(['register'=>false]);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/{page}', [AdminController::class, 'index']);
